249f5523d8b6475102e5c49cdadfe07962bb87ff
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2024-09-05 23:23:47 +0700

    add soal

---
+++ src/main.py
@@ -0,0 +1,14 @@
+def penjumlahan(a, b):
+    """
+    Menghitung penjumlahan dari dua angka.
+    :param a: angka pertama
+    :param b: angka kedua
+    :return: hasil penjumlahan a + b
+    """
+    # TODO: Implementasikan fungsi ini
+    pass
+
+
+# Contoh penggunaan (dapat dihapus jika diinginkan)
+if __name__ == "__main__":
+    print("Lengkapi fungsi penjumlahan!")
